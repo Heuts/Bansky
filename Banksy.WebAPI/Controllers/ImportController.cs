8212263faa1c607b107ec53cdcdc0bddbd4a0b78
Hugo <hugoheuts@gmail.com>
2020-01-23 21:41:26 +0100

    Added ImportController to API

---
+++ Banksy.WebAPI/Controllers/ImportController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Banksy.WebAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Banksy.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportController : ControllerBase
+    {
+        private ImportService importService;
+
+        public ImportController (ImportService importService)
+        {
+            this.importService = importService;
+        }
+
+        [DisableRequestSizeLimit]
+        public async Task<ActionResult> UploadFilesAsync()
+        {
+            try
+            {
+                IFormFile file = Request.Form.Files[0];
+
+                if (file.Length > 0)
+                {
+                    await Task.Run(() => importService.ImportExcel(file));
+                }
+                return Ok(file.Name);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
