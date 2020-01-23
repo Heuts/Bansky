d668a76e3c575d275a05e8ea975a5ce3fccd611d
Hugo <hugoheuts@gmail.com>
2020-01-23 21:41:40 +0100

    Added ImportService and interface to API

---
+++ Banksy.WebAPI/Services/ImportService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banksy.WebAPI.Services
+{
+    public class ImportService : IImportService
+    {
+        public void ImportExcel(IFormFile file)
+        {
+
+        }
+    }
+}
