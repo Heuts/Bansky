07c545ba20afd2a3182712fa8769581554198e6b
Hugo <hugoheuts@gmail.com>
2020-01-23 22:24:31 +0100

    Added DbContext and InitialCreate EF Core to API

---
+++ Banksy.WebAPI/Data/BanksyContext.cs
@@ -0,0 +1,18 @@
+﻿using Banksy.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banksy.WebAPI.Data
+{
+    public class BanksyContext : DbContext
+    {
+        public DbSet<Mutation> Mutations { get; set; }
+
+        public BanksyContext() : this(new DbContextOptions<BanksyContext>()) { }
+
+        public BanksyContext(DbContextOptions<BanksyContext> options) : base(options) { }
+    }
+}
