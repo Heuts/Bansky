0856c7244590a85f1eb9d90c1abce6bb5ab36826
Hugo <hugoheuts@gmail.com>
2020-01-24 22:21:49 +0100

    Fixed typo in entity property

--- Banksy.WebAPI/Migrations/20200123215744_TypeFix.Designer.cs
+++ Banksy.WebAPI/Migrations/20200123215744_TypeFix.Designer.cs
@@ -4,14 +4,16 @@ using Banksy.WebAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Banksy.WebAPI.Migrations
 {
     [DbContext(typeof(BanksyContext))]
-    partial class BanksyContextModelSnapshot : ModelSnapshot
+    [Migration("20200123215744_TypeFix")]
+    partial class TypeFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Banksy.WebAPI/Migrations/20200123215744_TypeFix.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Banksy.WebAPI.Migrations
+{
+    public partial class TypeFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MutaionType",
+                table: "Mutations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MutationType",
+                table: "Mutations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MutationType",
+                table: "Mutations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MutaionType",
+                table: "Mutations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
