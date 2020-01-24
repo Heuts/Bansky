50aa43d8b4d25b0cafb53283a056ab3debd031d3
Hugo <hugoheuts@gmail.com>
2020-01-24 22:22:26 +0100

    Added CSVReader to ImportService

--- Banksy.WebAPI/Services/ImportService.cs
+++ Banksy.WebAPI/Services/ImportService.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Banksy.WebAPI.Models;
+using CsvHelper;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +14,18 @@ namespace Banksy.WebAPI.Services
     {
         public void ImportExcel(IFormFile file)
         {
+            using (var memoryStream = new MemoryStream())
+            {
+                file.CopyToAsync(memoryStream);
+                memoryStream.Position = 0;
+                TextReader textReader = new StreamReader(memoryStream);
+                var csv = new CsvReader(textReader, new CultureInfo("nl-NL"));
+                csv.Configuration.RegisterClassMap<MutationMap>();
+                csv.Configuration.Delimiter = ",";
 
+                var records = csv.GetRecords<Mutation>();
+
+            }
         }
     }
 }
