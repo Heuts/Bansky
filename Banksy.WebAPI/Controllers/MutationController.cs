cf5f8d517b68a786b3ee9351a12b374148c741ec
Hugo <hugoheuts@gmail.com>
2020-01-25 16:05:29 +0100

    Added MutationController to API

---
+++ Banksy.WebAPI/Controllers/MutationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Banksy.WebAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Banksy.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MutationController : ControllerBase
+    {
+        private IMutationService mutationService;
+
+        public MutationController(IMutationService mutationService)
+        {
+            this.mutationService = mutationService;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllMutations()
+        {
+            return Ok(await mutationService.GetAllMutations());
+        }
+    }
+}
