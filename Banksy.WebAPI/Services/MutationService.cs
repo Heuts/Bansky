b18b9115e34372ea37cbfdf8f8289c26e4d18419
Hugo <hugoheuts@gmail.com>
2020-01-25 16:04:35 +0100

    Added MutationService and interface to API

---
+++ Banksy.WebAPI/Services/MutationService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Banksy.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banksy.WebAPI.Services
+{
+    public class MutationService : IMutationService
+    {
+        private BanksyContext context;
+        private IMapper mapper;
+
+        public MutationService(BanksyContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<List<DTOs.Mutation>> GetAllMutations()
+        {
+            return await context.Mutations.Select(m => mapper.Map<DTOs.Mutation>(m)).ToListAsync();
+        }
+    }
+}
