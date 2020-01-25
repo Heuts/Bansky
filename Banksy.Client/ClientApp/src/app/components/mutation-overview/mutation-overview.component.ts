6e2e6e0c2b88387329da3ca5a2045e04366b30d9
Hugo <hugoheuts@gmail.com>
2020-01-25 16:51:35 +0100

    Implemented mutation-overview component

--- Banksy.Client/ClientApp/src/app/components/mutation-overview/mutation-overview.component.ts
+++ Banksy.Client/ClientApp/src/app/components/mutation-overview/mutation-overview.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MutationService } from "src/app/services/mutation.service";
+import { MutationDTO } from "src/app/dtos/mutation.dto";
 
 @Component({
-  selector: 'app-mutation-overview',
-  templateUrl: './mutation-overview.component.html',
-  styleUrls: ['./mutation-overview.component.css']
+  selector: "app-mutation-overview",
+  templateUrl: "./mutation-overview.component.html",
+  styleUrls: ["./mutation-overview.component.css"]
 })
 export class MutationOverviewComponent implements OnInit {
+  public mutations: MutationDTO[];
 
-  constructor() { }
+  constructor(private mutationService: MutationService) {}
 
   ngOnInit() {
+    this.mutationService.getAllMutations().subscribe(m => (this.mutations = m));
   }
-
 }
