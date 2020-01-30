7e3a3b6fe5aee39b33d231dc20fd4ee2425d1eea
Hugo <hugoheuts@gmail.com>
2020-01-30 22:19:34 +0100

    Added mutation-detail component

---
+++ Banksy.Client/ClientApp/src/app/components/mutation-detail/mutation-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { map } from "rxjs/operators";
+import { MutationDTO } from "src/app/dtos/mutation.dto";
+import { MutationService } from "src/app/services/mutation.service";
+
+@Component({
+  selector: "app-mutation-detail",
+  templateUrl: "./mutation-detail.component.html",
+  styleUrls: ["./mutation-detail.component.css"]
+})
+export class MutationDetailComponent implements OnInit {
+  mutation: MutationDTO;
+  mutationId: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private mutationService: MutationService
+  ) {}
+
+  ngOnInit() {
+    this.getRouteParameter();
+    this.getMutation();
+  }
+
+  getRouteParameter() {
+    this.route.paramMap
+      .pipe(map(params => params.get("mutationId")))
+      .subscribe(m => (this.mutationId = +m));
+  }
+
+  getMutation() {
+    this.mutationService
+      .getMutation(this.mutationId)
+      .subscribe(m => (this.mutation = m));
+  }
+}
