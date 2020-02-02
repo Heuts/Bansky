2a57ac2929e2d7594580c23a8d9c8f83de9241fd
Hugo <hugoheuts@gmail.com>
2020-02-02 22:10:52 +0100

    Added data seeding for categories in dbcontext

--- Banksy.WebAPI/Data/BanksyContext.cs
+++ Banksy.WebAPI/Data/BanksyContext.cs
@@ -11,6 +11,22 @@ namespace Banksy.WebAPI.Data
     {
         public DbSet<Mutation> Mutations { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1 ,Name = "Housing" },
+                new Category { Id = 2, Name = "Transportation" },
+                new Category { Id = 3, Name = "Groceries" },
+                new Category { Id = 4, Name = "Utilities" },
+                new Category { Id = 5, Name = "Insurance" },
+                new Category { Id = 6, Name = "Medical and Healthcare" },
+                new Category { Id = 7, Name = "Saving, Investing and Debt payments" },
+                new Category { Id = 8, Name = "Personal spending" },
+                new Category { Id = 9, Name = "Recreation and Entertainment" },
+                new Category { Id = 10, Name = "Miscellaneous" }
+                );
+        }
+
         public BanksyContext() : this(new DbContextOptions<BanksyContext>()) { }
 
         public BanksyContext(DbContextOptions<BanksyContext> options) : base(options) { }
