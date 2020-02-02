34c4e4d0f378bc62692162c2bcb39ec80a27574a
Hugo <hugoheuts@gmail.com>
2020-02-02 22:10:29 +0100

    Added Category to Mutation model in API

---
+++ Banksy.WebAPI/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banksy.WebAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Category> SubCategories { get; set; }
+    }
+}
