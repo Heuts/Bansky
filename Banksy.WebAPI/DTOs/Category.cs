1461fb121df339c32d9eb61848f15b69da1f3e54
Hugo <hugoheuts@gmail.com>
2020-02-05 21:24:01 +0100

    Added Category DTO to API

---
+++ Banksy.WebAPI/DTOs/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Banksy.WebAPI.DTOs
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int[] SubCategoryIds { get; set; }
+    }
+}
