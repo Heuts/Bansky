6319b5a5c6cd6844f5d4cbde43d2bde4a801b850
Hugo Heuts <hugoheuts@gmail.com>
2021-01-06 17:14:06 +0100

    Moved interfaces to distinct folder

--- Banksy.WebAPI/Services/Interfaces/IImportService.cs
+++ Banksy.WebAPI/Services/Interfaces/IImportService.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
-namespace Banksy.WebAPI.Services
+namespace Banksy.WebAPI.Services.Interfaces
 {
     public interface IImportService
     {
