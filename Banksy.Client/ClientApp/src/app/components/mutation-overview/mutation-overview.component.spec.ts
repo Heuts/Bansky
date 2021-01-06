eeb1572841c7ea67fd2c2788e5b0bd93d5ddc041
Hugo Heuts <hugoheuts@gmail.com>
2021-01-06 23:29:14 +0100

    Updated client to Angular 11

--- Banksy.Client/ClientApp/src/app/components/mutation-overview/mutation-overview.component.spec.ts
+++ Banksy.Client/ClientApp/src/app/components/mutation-overview/mutation-overview.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MutationOverviewComponent } from './mutation-overview.component';
 
@@ -6,7 +6,7 @@ describe('MutationOverviewComponent', () => {
   let component: MutationOverviewComponent;
   let fixture: ComponentFixture<MutationOverviewComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MutationOverviewComponent ]
     })
