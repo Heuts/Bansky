66a573e26301ec04bf9413ff398beb643e0034c4
Hugo Heuts <hugoheuts@gmail.com>
2021-01-06 17:18:29 +0100

    Removed unused imports in StartUp

--- Banksy.WebAPI/Startup.cs
+++ Banksy.WebAPI/Startup.cs
@@ -1,19 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using Banksy.WebAPI.Data;
 using Banksy.WebAPI.Services;
+using Banksy.WebAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Banksy.WebAPI
 {
